890d0a73327657f6c2bc4708b6f7480b85e72d11
Ivan Gamboa <chavez.ivan@correounivalle.edu.co>
2025-11-18 01:20:24 -0500

    Corrigiendo

--- backend/routes/vehiculos.routes.js
+++ backend/routes/vehiculos.routes.js
@@ -2,17 +2,21 @@ import { Router } from "express";
 const router = Router();
 
 // POST /api/vehiculos/registro
-router.post("/registro", (req, res) => {
-    const { placa, modelo, tipo } = req.body;
-
-    if (!placa || !modelo || !tipo) {
-        return res.status(400).json({ message: "Faltan datos" });
-    }
-
-    res.status(201).json({
-        message: "Vehículo registrado correctamente",
-        datos: { placa, modelo, tipo }
-    });
-});
-
 export default router;
+router.post("/registro", async (req, res) => {
+    console.log("Body recibido:", req.body); // <--- IMPORTANTE
+
+    const { placa, modelo, tipo, capacidad } = req.body;
+
+    try {
+        const nuevoVehiculo = await pool.query(
+            "INSERT INTO vehiculos (placa, modelo, tipo, capacidad) VALUES ($1, $2, $3, $4) RETURNING *",
+            [placa, modelo, tipo, capacidad]
+        );
+
+        res.json(nuevoVehiculo.rows[0]);
+    } catch (error) {
+        console.error("Error en INSERT:", error);  // <---- VER EL ERROR REAL
+        res.status(500).json({ error: "Error al registrar vehículo" });
+    }
+});
