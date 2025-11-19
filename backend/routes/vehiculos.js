567bfe7a47d2ad7e35e94423cddc0e5295ade69d
Ivan Gamboa <chavez.ivan@correounivalle.edu.co>
2025-11-19 11:32:34 -0500

    editar-vehiculos

--- backend/routes/vehiculos.js
+++ backend/routes/vehiculos.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { registrarVehiculo, obtenerVehiculos } = require('../controllers/vehiculosController');
+
+const { 
+  registrarVehiculo, 
+  obtenerVehiculos, 
+  actualizarVehiculo 
+} = require('../controllers/vehiculosController');
 
 // Registrar vehículo
 router.post('/registro', registrarVehiculo);
 
 // Listar vehículos
 router.get('/', obtenerVehiculos);
+
+// Editar vehículo
+router.put('/editar/:id', actualizarVehiculo);
 
 module.exports = router;
